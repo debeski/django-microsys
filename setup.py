c983df0f29346e1e2d20f3460c7ff64f6d1072d7
debeski <debeski1@gmail.com>
2025-12-03 16:43:42 +0200

    pypi+

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="micro_users",
-    version="1.0.1",
+    version="1.0.2",
     author="DeBeski",
     author_email="debeski1@gmail.com",
-    description="Django user management app with abstract user, permissions, and activity logging",
+    description="Arabic django user management app with abstract user, permissions, and activity logging",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/debeski/micro_users",
+    url="https://github.com/debeski/micro-users",
     packages=["users"],
     include_package_data=True,
     classifiers=[
@@ -23,15 +23,15 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "License :: OSI Approved :: MIT License",  # MIT license
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
     install_requires=[
         "Django>=5.17",
         "django-crispy-forms>=2.4",
-        "django-tables2>=2.7.5",
-        "django-filter>=25.1",
+        "django-tables2>=2.7",
+        "django-filter>=24.3",
         "pillow>=11.0",
         "babel>=2.1",
     ],
